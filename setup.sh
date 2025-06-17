e22d339fd28b680aba04fd5e4f4f80e907495910
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-17 22:27:57 +0200

    Remove custom env file logic

--- setup.sh
+++ setup.sh
@@ -8,15 +8,15 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 
-# This can be re-entered manually work through
-# the logic in the `data-loading` directory.
+# This can be re-entered manually; work through
+# the logic in the `data-loading` directory's
+# README.md.
 deactivate
 
 # Clone the diffusion model repository.
 # https://github.com/xuekt98/BBDM
 git clone https://github.com/xuekt98/BBDM.git
 
-# Setup the BBDM Conda environment, using more
-# publicly available mirrors
-cd configs
-conda env create -f bbdm-environment.yml
+# Setup the "BBDM" Conda environment
+cd BBDM
+conda env create -f environment.yml
