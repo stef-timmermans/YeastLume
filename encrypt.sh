6c0b1cc49bf07450c8284844852bede891f24773
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-23 14:51:01 +0200

    Correct SOPS logic

--- encrypt.sh
+++ encrypt.sh
@@ -5,10 +5,11 @@
 set -e
 
 # Create a SOPs key file (ignored from tracking)
-sops age-keygen > accounts/key
+age-keygen -o accounts/key
 
 # Use the key file to create an encrypted version
 # of the GCP configuration that can be safely put
 # in version control.
 AGE_KEY=$(grep "public key:" accounts/key | awk '{print $NF}')
 sops --encrypt --age "$AGE_KEY" accounts/drive.json > accounts/drive.json.enc
+
