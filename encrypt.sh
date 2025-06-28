ab9a6bebdb56237887ef5cd4828cd3f3f9df42ab
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-28 21:20:20 +0200

    Prettify recent decrypt logic

--- encrypt.sh
+++ encrypt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Leave the script if an error is encountered.
+# Leave the script if an error is encountered
 
 #!/bin/bash
 set -e
@@ -9,7 +9,6 @@ age-keygen -o accounts/key
 
 # Use the key file to create an encrypted version
 # of the GCP configuration that can be safely put
-# in version control.
+# in version control
 AGE_KEY=$(grep "public key:" accounts/key | awk '{print $NF}')
 sops --encrypt --age "$AGE_KEY" accounts/drive.json > accounts/drive.json.enc
-
