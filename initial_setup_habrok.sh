ecb9a1cc634769202fa70c269a1401bce3a15838
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-29 21:01:39 +0200

    Conda fix and a half

--- initial_setup_habrok.sh
+++ initial_setup_habrok.sh
@@ -2,39 +2,37 @@
 # Leave the script if an error is encountered
 set -e
 
-# Purge old modules
-module purge
-
 # Set the data data working directory
 DATA_DIR="$(pwd)/data"
 export DATA_DIR
 
-# Clone the diffusion model repository and remove it from tracking
+# Clone the diffusion model
 # https://github.com/xuekt98/BBDM
 git clone https://github.com/xuekt98/BBDM.git
+
+# Overwrite the BBDM environment file for Hábrók fix
+mv environment.yml BBDM/
 cd BBDM
+
+# Remote tracking from sub-repository
 rm -rf .git
 
 # Navigate to the config files
 cd configs
 
-# Support macOS and Linux calls for `sed`
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED_EXT=(-i '')
-else
-    SED_EXT=(-i)
-fi
-
 # Overwrite the BBDM template files' target input data path
-sed "${SED_EXT[@]}" "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-BBDM.yaml
-sed "${SED_EXT[@]}" "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f4.yaml
-sed "${SED_EXT[@]}" "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f8.yaml
-sed "${SED_EXT[@]}" "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f16.yaml
+sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-BBDM.yaml
+sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f4.yaml
+sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f8.yaml
+sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f16.yaml
 cd ..
 
-# Load in a compatible Python version for BBDM
-module load Python/3.9.6-GCCcore-11.2.0
-
-# Setup the "BBDM" Conda environment
+# Install Conda
+module purge
 module load Anaconda3/2024.02-1
-conda env create -f environment.yml
+conda --version
+
+# Set up the BBDM environment
+conda create -n BBDM python=3.9.16 -y
+conda activate BBDM
+conda env update --file environment.yml --prune
