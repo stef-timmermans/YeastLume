ffb3b6a44aceeaaa6105394283db256755373350
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-29 02:48:18 +0200

    Fix BBDM config data path overwrite

--- initial_setup.sh
+++ initial_setup.sh
@@ -16,18 +16,30 @@ pip install -r requirements.txt
 # README.md.
 deactivate
 
+# Set the data data working directory
+DATA_DIR="$(pwd)/data"
+export DATA_DIR
+
 # Clone the diffusion model repository and remove it from tracking
 # https://github.com/xuekt98/BBDM
 git clone https://github.com/xuekt98/BBDM.git
 cd BBDM
 rm -rf .git
 
-# Tweak the config file to correctly load in the user-provided dataset
+# Navigate to the config files
 cd configs
-sed -i '' "19s|dataset_path: '.*'|dataset_path: '../../data'|" Template-BBDM.yaml
-sed -i '' "19s|dataset_path: '.*'|dataset_path: '../../data'|" Template-LBBDM-f4.yaml
-sed -i '' "19s|dataset_path: '.*'|dataset_path: '../../data'|" Template-LBBDM-f8.yaml
-sed -i '' "19s|dataset_path: '.*'|dataset_path: '../../data'|" Template-LBBDM-f16.yaml
+
+# Support macOS and Linux calls for `sed`
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_EXT="-i ''"
+else
+    SED_EXT="-i"
+fi
+
+sed ${SED_EXT} '' "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-BBDM.yaml
+sed ${SED_EXT} '' "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f4.yaml
+sed ${SED_EXT} '' "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f8.yaml
+sed ${SED_EXT} '' "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" Template-LBBDM-f16.yaml
 cd ..
 
 # Setup the "BBDM" Conda environment
