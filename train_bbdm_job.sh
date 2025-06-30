094f7d22c93b032670e46315f39a305a039c9372
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-06-30 12:51:24 +0200

    Double job length due to smaller batch size

--- train_bbdm_job.sh
+++ train_bbdm_job.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=logs/bbdm_yeast_train_%j.out
 #SBATCH --error=logs/bbdm_yeast_train_%j.err
 
