d6e64d7e3ca06372bda657ad193f191ee3d55bf5
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-07-06 01:43:05 +0200

    Give more time for eval (takes significant time for many images)

--- scripts/jobs/eval_bbdm_job.sh
+++ scripts/jobs/eval_bbdm_job.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --time=4:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=logs/bbdm_yeast_eval_%j.out
 #SBATCH --error=logs/bbdm_yeast_eval_%j.err
 
