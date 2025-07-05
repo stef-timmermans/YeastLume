d2f4e00f521158759e5f48394bcfdaef59de6038
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-07-05 23:41:41 +0200

    Log to *_eval_%j for eval scrips

--- scripts/jobs/eval_bbdm_job.sh
+++ scripts/jobs/eval_bbdm_job.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=4:00:00
-#SBATCH --output=logs/bbdm_yeast_train_%j.out
-#SBATCH --error=logs/bbdm_yeast_train_%j.err
+#SBATCH --output=logs/bbdm_yeast_eval_%j.out
+#SBATCH --error=logs/bbdm_yeast_eval_%j.err
 
 # Leave the script if an error is encountered
 set -e
