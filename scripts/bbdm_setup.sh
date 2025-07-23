c78b4392d359afbcec3ca3f750eda58659ead54c
Stef Timmermans <stef.timmermans.pdx@gmail.com>
2025-07-23 19:18:40 +0200

    Update BBDM setup

--- scripts/bbdm_setup.sh
+++ scripts/bbdm_setup.sh
@@ -2,6 +2,22 @@
 # Leave the script if an error is encountered
 set -e
 
+# Load .env config
+if [ -f .env ]; then
+    set -a
+    source .env
+    set +a
+else
+    echo "❌ .env file not found in project root. Exiting..."
+    exit 1
+fi
+
+# Ensure COLOR_MODE is set
+if [ -z "$COLOR_MODE" ]; then
+  echo "❌ COLOR_MODE not set in .env"
+  exit 1
+fi
+
 echo "✅ Starting BBDM setup..."
 
 # Set the data working directory
@@ -13,30 +29,43 @@ echo "✅ Set DATA_DIR to $DATA_DIR"
 VQGAN_DIR="$(pwd)"/checkpoints/VQGAN/last.ckpt
 echo "✅ Set VQGAN_DIR to $VQGAN_DIR"
 
-# Overwrite the BBDM template file data and checkpoint paths
-sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" configs/models/rgb/Template-LBBDM-f4.yaml
-echo "✅ Updated dataset_path in BBDM yaml"
-
-sed -i "56s|ckpt_path: '.*'|ckpt_path: '${VQGAN_DIR}'|" configs/models/rgb/Template-LBBDM-f4.yaml
-echo "✅ Updated ckpt_path in BBDM yaml"
-
 # Clone the diffusion model if it doesn't already exist
 # https://github.com/xuekt98/BBDM
 if [ -d "BBDM" ]; then
-    echo "⚠️BBDM directory already exists; skipping clone"
+    echo "⚠️ BBDM directory already exists; skipping clone"
 else
     git clone https://github.com/xuekt98/BBDM.git
     echo "✅ Cloned BBDM repository"
 fi
 
-# Overwrite the model instruction template
-cp configs/models/rgb/Template-LBBDM-f4.yaml BBDM/configs/
+# Set up the appropriate configuration file
+# Set up for grayscale
+if [ "$COLOR_MODE" = "grayscale" ]; then
+    echo "Setting up for grayscale..."
 
-# Revert root template to prevent committing user paths
-git checkout -- configs/models/rgb/Template-LBBDM-f4.yaml
+    # Overwrite the BBDM template file data and checkpoint paths
+    cp configs/models/rgb/Template-LBBDM-f4.yaml BBDM/configs/Template-LBBDM-f4.yaml
+
+    sed -i "19s|dataset_path: '.*'|dataset_path: '${DATA_DIR}'|" BBDM/configs/Template-LBBDM-f4.yaml
+    echo "✅ Updated dataset_path in BBDM yaml"
+
+    sed -i "56s|ckpt_path: '.*'|ckpt_path: '${VQGAN_DIR}'|" BBDM/configs/Template-LBBDM-f4.yaml
+    echo "✅ Updated ckpt_path in BBDM yaml"
+
+# Set up for rgb
+elif [ "$COLOR_MODE" = "rgb" ]; then
+    echo "Setting up for rgb"
+    # TODO
+
+# Catch error
+else
+    echo "❌ Unsupported COLOR_MODE: $COLOR_MODE"
+    exit 1
+fi
+
 
 cd BBDM
-echo "✅ Copied Template-LBBDM-f4.yaml; reverted repo root git changes; changed into BBDM directory"
+echo "✅ Copied Template-LBBDM-f4.yaml; applied user-specific file paths; changed into BBDM directory"
 
 # Remote tracking from sub-repository
 rm -rf .git
